ce25b70b2f3845eb46890dd01a3825cfc82648d5
Harpreet <investorhs09@gmail.com>
2022-04-15 00:26:58 -0400

    How to creat the xpaths

--- hw2locator_practice.py
+++ hw2locator_practice.py
@@ -7,18 +7,22 @@ driver = webdriver.Chrome()
 
 
 
-# # by XPATH(amazon logo, need help)
-# driver.find_element(By.XPATH, "//a[@class='a-icon a-icon-logo']")
-# driver.find_element(By.XPATH, "//span[@class='a-expander-prompt']")
-#
+# by XPATH(amazon logo, need help)
+driver.find_element(By.XPATH, "//a[@class='a-icon a-icon-logo']")
+driver.find_element(By.XPATH, "//span[@class='a-expander-prompt']")
+
+# BY class using x path
+driver.find_element(By.XPATH, " *[@class = 'icp-new-nav-flag']")
 
 
-# # search box and continue button
-# driver.find_element(By.ID, 'ap_email')
-# driver.find_element(By.ID, 'continue')
 
-# # Condition of use
-#driver.find_element(By.XPATH, "a[@herf='/gp/help/customer/display.html/ref=ap_signin_notification_condition_of_use?ie=UTF8&nodeId=508088']")
+
+# search box and continue button
+driver.find_element(By.ID, 'ap_email')
+driver.find_element(By.ID, 'continue')
+
+# Condition of use
+driver.find_element(By.XPATH, "a[@herf='/gp/help/customer/display.html/ref=ap_signin_notification_condition_of_use?ie=UTF8&nodeId=508088']")
 
 
 
