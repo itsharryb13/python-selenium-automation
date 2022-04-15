b8c662ce531585a4b86806a04f3a8defaa2bba14
Harpreet <investorhs09@gmail.com>
2022-04-14 22:15:55 -0400

    Homework 2

--- hw3_part3_testcase.py
+++ hw3_part3_testcase.py
@@ -4,7 +4,7 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 
 # init driver
-driver = webdriver.Chrome(executable_path='/Users/bavaharpreetbhalla/internship/python-selenium-automation/chromedriver')
+driver = webdriver.Chrome(executable_path='/Users/bavaharpreetbhalla/internship/main.sunday/chromedriver')
 driver.maximize_window()
 
 driver.get('https://www.amazon.com/gp/help/customer/display.html')
@@ -18,10 +18,11 @@ search.send_keys('Cancel Order')
 search.send_keys(Keys.RETURN)
 
 #verify
-expected_result = 'Cancel Order'
-actual_result= driver.find_element(By.XPATH,"/html/body/div[1]/div[2]/div[2]/div[1]/div/div[1]/div/div/form/div/p/b").text
+expected_result = 'Cancel Items or Orders'
+actual_result = driver.find_element(By.XPATH,"//div[@class ='help-content']/h1").text
 
 assert expected_result == actual_result, f"expected text {expected_result} is same as {actual_result}"
+sleep(4)
 print('Test Passed')
 driver.quit()
 
